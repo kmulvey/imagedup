3437518d9434202dbb460d99d0361fcaf5ce6c8e
Kevin Mulvey <kmulvey@linux.com>
2021-01-17 12:23:48 -0700

    bench

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.15
 require (
 	github.com/corona10/goimagehash v1.0.3
 	github.com/dgraph-io/badger/v3 v3.2011.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/prometheus/client_golang v1.9.0
 	github.com/sirupsen/logrus v1.7.0
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
 )
---
+++ main_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+	"testing"
+
+	"github.com/corona10/goimagehash"
+	"github.com/corona10/goimagehash/transforms"
+	"github.com/nfnt/resize"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/image/draw"
+)
+
+func TestDupNames(t *testing.T) {
+	var dir = "/home/kmulvey/Documents"
+	var files, err = listFiles(dir)
+	assert.NoError(t, err)
+
+	var static bool
+	var fmap = make(map[string]bool)
+	for _, f := range files {
+		//var filename = strings.Replace(f, dir, "", 1)
+		if _, ok := fmap[f]; !ok {
+			fmap[f] = static
+		} else {
+			fmt.Println(f)
+		}
+	}
+}
+
+func BenchmarkPerceptionHash(b *testing.B) {
+
+	var file, err = os.Open("/home/kmulvey/Documents/Valeria Mavrin.jpg")
+	assert.NoError(b, err)
+
+	img, err := jpeg.Decode(file)
+	assert.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		hash, err := goimagehash.PerceptionHash(img)
+		assert.NoError(b, err)
+		assert.Equal(b, uint64(0x9c99caa3dc4a3476), hash.GetHash())
+	}
+}
+
+func BenchmarkNewHash(b *testing.B) {
+
+	var file, err = os.Open("/home/kmulvey/Documents/Valeria Mavrin.jpg")
+	assert.NoError(b, err)
+
+	img, err := jpeg.Decode(file)
+	assert.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		hash, err := Hash(img)
+		assert.NoError(b, err)
+		assert.Equal(b, uint64(0x9c994aa3de4a7076), hash.GetHash())
+	}
+}
+
+func BenchmarkNfntScale(b *testing.B) {
+	var file, err = os.Open("/home/kmulvey/Documents/Valeria Mavrin.jpg")
+	assert.NoError(b, err)
+
+	img, err := jpeg.Decode(file)
+	assert.NoError(b, err)
+
+	for i := 0; i < b.N; i++ {
+		var small = resize.Resize(64, 64, img, resize.NearestNeighbor)
+		transforms.Rgb2Gray(small)
+	}
+}
+
+func BenchmarkXScale(b *testing.B) {
+	var file, err = os.Open("/home/kmulvey/Documents/Valeria Mavrin.jpg")
+	assert.NoError(b, err)
+
+	img, err := jpeg.Decode(file)
+	assert.NoError(b, err)
+
+	sr := img.Bounds()
+	dr := image.Rect(0, 0, 64, 64)
+	dst := image.NewRGBA(dr)
+
+	for i := 0; i < b.N; i++ {
+		draw.NearestNeighbor.Scale(dst, dr, img, sr, draw.Src, nil)
+		transforms.Rgb2Gray(dst)
+	}
+}
