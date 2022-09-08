905849df4daa5c106dcad81c9ad758f9852cb0a6
Kevin Mulvey <kmulvey@linux.com>
2022-09-08 16:32:53 -0600

    hook up everything in main

--- cmd/nsquared/main.go
+++ cmd/nsquared/main.go
@@ -1,10 +1,28 @@
 package main
 
-/*
-const hashCacheFile = "hashcache.json"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/kmulvey/imagedup/internal/app/imagedup"
+	"github.com/kmulvey/path"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
+)
 
 func main() {
 	var start = time.Now()
+	var ctx, cancel = context.WithCancel(context.Background())
 
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
@@ -22,9 +40,13 @@ func main() {
 
 	// get user opts
 	var rootDir string
+	var cacheFile string
+	var outputFile string
 	var threads int
 	var distanceThreshold int
 	flag.StringVar(&rootDir, "dir", "", "directory (abs path)")
+	flag.StringVar(&cacheFile, "cache-file", "cache.json", "json file to store the image hashes")
+	flag.StringVar(&outputFile, "output-file", "delete.log", "log file to store the duplicate pairs")
 	flag.IntVar(&threads, "threads", 1, "number of threads to use, >1 only useful when rebuilding the cache")
 	flag.IntVar(&distanceThreshold, "distance", 10, "max distance for images to be considered the same")
 	flag.Parse()
@@ -34,13 +56,26 @@ func main() {
 	if threads <= 0 || threads > runtime.GOMAXPROCS(0) {
 		threads = 1
 	}
+	if filepath.Ext(cacheFile) != "json" {
+		log.Fatal("cache file must have extension .json")
+	}
+	if filepath.Ext(outputFile) != "log" {
+		log.Fatal("output file must have extension .log")
+	}
 
-	var ctx, cancel = context.WithCancel(context.Background())
-	var pairChan = make(chan types.Pair)
-	var files, imageHashCache, dp = setup(ctx, rootDir, threads, distanceThreshold, pairChan)
-	go stream.StreamFiles(ctx, files, pairChan)
+	// start er up
+	var id, err = imagedup.NewImageDup(ctx, "imagedup", cacheFile, outputFile, threads, distanceThreshold)
+	handleErr("NewImageDup", err)
+
+	// list all the files
+	files, err := path.ListFilesWithFilter(rootDir, regexp.MustCompile(".*.jpg$|.*.jpeg$|.*.png$.*.webm$"))
+	handleErr("listFiles", err)
+	files = path.OnlyDirs(files)
+	var fileNames = path.OnlyNames(files)
+	log.Infof("Found %d dirs", len(files))
 
 	log.Info("Started, go to grafana to monitor")
+	id.Start(fileNames)
 
 	// wait for all diff workers to finish or we get a shutdown signal
 	// whichever comes first
@@ -49,7 +84,7 @@ func main() {
 		select {
 		case <-gracefulShutdown:
 			graceful = false
-		case <-dp.Wait():
+		case <-id.Wait():
 			workers = false
 		}
 	}
@@ -57,51 +92,12 @@ func main() {
 	// shut everything down
 	log.Info("Shutting down")
 	cancel()
-	var err = shutdown(imageHashCache)
+	err = id.Shutdown(cacheFile)
 	if err != nil {
 		log.Fatal("error shutting down", err)
 	}
 
 	log.Info("Total time taken: ", time.Since(start))
-}
-
-func setup(ctx context.Context, rootDir string, threads, distanceThreshold int, pairChan chan types.Pair) ([]string, *cache.HashCache, *diffpool.DiffPool) {
-
-	var deleteLogger = logger.NewDeleteLogger()
-
-	// list all the files
-	var files, err = path.ListFilesWithFilter(rootDir, regexp.MustCompile(".*.jpg$|.*.jpeg$|.*.png$.*.webm$"))
-	handleErr("listFiles", err)
-	files = path.OnlyDirs(files)
-	var fileNames = path.OnlyNames(files)
-	log.Infof("Found %d dirs", len(files))
-
-	// init the image cache
-	imageHashCache, err := cache.NewHashCache(hashCacheFile)
-	handleErr("NewHashCache", err)
-	log.Infof("Loaded %d image hashes from disk cache", len(imageHashCache.Cache))
-
-	// init diff workers
-	var dp = diffpool.NewDiffPool(ctx, threads, distanceThreshold, pairChan, imageHashCache, deleteLogger)
-
-	// init prom
-	go publishStats(imageHashCache)
-
-	// starter stats
-	totalComparisons.Set(float64(len(files) * (len(files) - 1)))
-
-	return fileNames, imageHashCache, dp
-}
-
-// shutdown gracefully shuts everything down and stores caches for next time
-func shutdown(cache *cache.HashCache) error {
-
-	var err = cache.Persist(hashCacheFile)
-	if err != nil {
-		return err
-	}
-
-	return cache.Persist(hashCacheFile)
 }
 
 // handleErr is a convience func to log and quit errors, all errors in this app are considered fatal
@@ -110,4 +106,3 @@ func handleErr(prefix string, err error) {
 		log.Fatal(fmt.Errorf("%s: %w", prefix, err))
 	}
 }
-*/
