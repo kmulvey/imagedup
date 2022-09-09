748e4356bceb060121ac768c0d8f00112851258c
Kevin Mulvey <kmulvey@linux.com>
2022-09-09 14:57:50 -0600

    field alignment

--- pkg/types/pair.go
+++ pkg/types/pair.go
@@ -3,8 +3,8 @@ package types
 // Pair represents two images, their paths and their element # in the files list
 // 48 bytes each
 type Pair struct {
-	I   int
-	J   int
 	One string
 	Two string
+	I   int
+	J   int
 }
